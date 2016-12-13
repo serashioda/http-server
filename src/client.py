483f9c06132ab9c5697511ec088723e2d404879c
Your Name <you@example.com>
2016-12-12 18:37:47 -0800

    server/client connection made

--- src/client.py
+++ src/client.py
@@ -3,7 +3,11 @@
 import socket
 
 
-server_info = socket.getaddrinfo('127.0.0.1', 4000)
-stream_info = [i for i in server_info if i[1] == socket.SOCK_STREAM][0]
-client = socket.socket(*stream_info[:3])
-client.connect(stream_info[-1])
+def create_client_socket():
+    """Function builds the client socket."""
+    server_info = socket.getaddrinfo('127.0.0.1', 4000)
+    stream_info = [i for i in server_info if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
+
+create_client_socket()
--- src/server.py
+++ src/server.py
@@ -2,7 +2,23 @@
 
 import socket
 
-
 server = socket.socket()
 address = ('127.0.0.1', 4000)
 server.bind(address)
+conn, addr = server.accept()
+
+# def create_server_socket():
+#     """Function builds server socket."""
+
+
+def server_listen():
+    """Function listens for client messages."""
+    buffer_length = 8
+    message_complete = False
+    while not message_complete:
+        part = conn.recv(buffer_length)
+        print(part.decode('utf8'))
+        if len(part) < buffer_length:
+            break
+
+server_listen()
