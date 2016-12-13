c47db53d591abca0296e2c8893a08708d5b806cd
Your Name <you@example.com>
2016-12-12 17:37:46 -0800

    server-client connected

--- src/client.py
+++ src/client.py
@@ -4,3 +4,6 @@ import socket
 
 
 server_info = socket.getaddrinfo('127.0.0.1', 4000)
+stream_info = [i for i in server_info if i[1] == socket.SOCK_STREAM][0]
+client = socket.socket(*stream_info[:3])
+client.connect(stream_info[-1])
