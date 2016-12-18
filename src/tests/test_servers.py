c7976b1d27d6b2e9a5d07b8c7abdda0d725c8cad
Your Name <you@example.com>
2016-12-17 18:42:51 -0800

    added more tests. todo's added.

--- src/tests/test_servers.py
+++ src/tests/test_servers.py
@@ -6,17 +6,36 @@ content_type = 'text/plain'
 
 
 def test_response_ok():
-    """Test response_ok."""
+    """Test response_ok when request is valid."""
     from server import response_ok
     result = response_ok(body_content, content_type)
     assert result == 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 20\r\n\r\n\r\nI am a body content.'
 
 
 def test_response_error():
-    """Test response_error."""
-    from server import response_error, ERRORS
+    """Test response_error when request is invalid."""
+    from server import response_error
     result = response_error('406')
     assert result == 'HTTP/1.1 406 Not Acceptable\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
+
+
+def test_parse_request():
+    """Test that uri is returned if request is valid."""
+    from server import parse_request
+    result = parse_request('GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n')
+    assert result == '/index.html'
+
+"""TODO:
+** test response_error:
+- test all error codes, and non-existing code (ie. 500), and when no error code passed in.
+
+** test resolve_uri:
+- 
+
+**overall functionality tests
+
+** parse_error convert to http
+** client stops receiving @ end of message (when len(body_content) is reached)
 
 
 # def test_client_for_405_response():
@@ -38,4 +57,3 @@ def test_response_error():
 #     from client import create_client_socket
 #     response = create_client_socket("GET /index.html HTTP/1.0\r\nHost: www.example.com\r\n\r\n")
 #     assert response == 'HTTP/1.1 406 Not Acceptable\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
-
