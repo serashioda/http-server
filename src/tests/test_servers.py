db9b4375d1cdf4706c51c3c476d90c74af19c444
Your Name <you@example.com>
2016-12-17 18:25:33 -0800

    tests for response_ok, response_error. Passing.

--- src/tests/test_servers.py
+++ src/tests/test_servers.py
@@ -1,37 +1,41 @@
 # # -*- coding: utf-8 -*-
 """Test client socket with various messages."""
 
-def test_response_ok(""):
+body_content = 'I am a body content.'
+content_type = 'text/plain'
+
+
+def test_response_ok():
     """Test response_ok."""
     from server import response_ok
-    result = response_ok()
-    assert result == 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
+    result = response_ok(body_content, content_type)
+    assert result == 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 20\r\n\r\n\r\nI am a body content.'
 
 
-def test_client_for_ok_response():
-    """Test for ok response from server with empty message."""
-    from client import create_client_socket
-    response = create_client_socket("GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n")
-    assert response == 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
+def test_response_error():
+    """Test response_error."""
+    from server import response_error, ERRORS
+    result = response_error('406')
+    assert result == 'HTTP/1.1 406 Not Acceptable\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
 
 
-def test_client_for_405_response():
-    """Test for ok response from server with empty message."""
-    from client import create_client_socket
-    response = create_client_socket("GOT /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n")
-    assert response == 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
+# def test_client_for_405_response():
+#     """Test for ok response from server with empty message."""
+#     from client import create_client_socket
+#     response = create_client_socket("GOT /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n")
+#     assert response == 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
 
 
-def test_client_for_400_response():
-    """Test for ok response from server with empty message."""
-    from client import create_client_socket
-    response = create_client_socket("GET /index.html HTTP/1.1\r\nPost: www.example.com\r\n\r\n")
-    assert response == 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
+# def test_client_for_400_response():
+#     """Test for ok response from server with empty message."""
+#     from client import create_client_socket
+#     response = create_client_socket("GET /index.html HTTP/1.1\r\nPost: www.example.com\r\n\r\n")
+#     assert response == 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
 
 
-def test_client_for_406_response():
-    """Test for ok response from server with empty message."""
-    from client import create_client_socket
-    response = create_client_socket("GET /index.html HTTP/1.0\r\nHost: www.example.com\r\n\r\n")
-    assert response == 'HTTP/1.1 406 Not Acceptable\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
+# def test_client_for_406_response():
+#     """Test for ok response from server with empty message."""
+#     from client import create_client_socket
+#     response = create_client_socket("GET /index.html HTTP/1.0\r\nHost: www.example.com\r\n\r\n")
+#     assert response == 'HTTP/1.1 406 Not Acceptable\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n'
 
